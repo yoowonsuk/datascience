4b7e8f7113b522e6a3f45cf3ff40d5b5e8d27076
yoowonsuk <sam2yuk@naver.com>
2020-04-05 22:38:59 +0900

    final

--- fourlayertrial.py
+++ fourlayertrial.py
@@ -25,35 +25,35 @@ class Model(nn.Module):
         #                  TODO : Forward path를 위한 Linear함수 또는 Weight 와 bias를 정의                           #
         ###############################################################################################################
 
-        self.seed = torch.manual_seed(139108) #
+        self.seed = torch.manual_seed(1393204) # make random num
 
         # self.lin1 = nn.Linear(input_size, output_size) # for layer 1
 
         self.lin1 = nn.Linear(input_size, H1) # Affine layer(input size, output_size), for layer 2 and 4
-        self.lin2 = nn.Linear(input_size, output_size)
+        # self.lin2 = nn.Linear(input_size, output_size)"""
 
-        """self.lin2 = torch.nn.Linear(H1, H1) # for layer 4
+        self.lin2 = torch.nn.Linear(H1, H1) # for layer 4
         self.lin3 = torch.nn.Linear(H1, H1)
         self.lin4 = torch.nn.Linear(H1, output_size)
-        torch.nn.init.xavier_uniform(self.lin1.weight) # weight initialzation by Xavier
-        torch.nn.init.xavier_uniform(self.lin2.weight)
-        torch.nn.init.xavier_uniform(self.lin3.weight)
-        torch.nn.init.xavier_uniform(self.lin4.weight)"""
-        
-        """torch.nn.init.xavier_normal(self.lin1.weight) # weight initialzation by Xavier
-        torch.nn.init.xavier_normal(self.lin2.weight) 
-        torch.nn.init.xavier_normal(self.lin3.weight) 
-        torch.nn.init.xavier_normal(self.lin4.weight) """
+        #torch.nn.init.xavier_uniform(self.lin1.weight) # weight initialzation by Xavier
+        #torch.nn.init.xavier_uniform(self.lin2.weight)
+        #torch.nn.init.xavier_uniform(self.lin3.weight)
+        #torch.nn.init.xavier_uniform(self.lin4.weight) 
 
-        """torch.nn.init.uniform_(self.lin1.weight, 0, 0.01)
+        #torch.nn.init.xavier_normal(self.lin1.weight) # weight initialzation by Xavier
+        #torch.nn.init.xavier_normal(self.lin2.weight) 
+        #torch.nn.init.xavier_normal(self.lin3.weight) 
+        #torch.nn.init.xavier_normal(self.lin4.weight) 
+
+        torch.nn.init.uniform_(self.lin1.weight, 0, 0.01)
         torch.nn.init.uniform_(self.lin2.weight, 0, 0.01)
         torch.nn.init.uniform_(self.lin3.weight, 0, 0.01)
-        torch.nn.init.uniform_(self.lin4.weight, 0, 0.01)"""
+        torch.nn.init.uniform_(self.lin4.weight, 0, 0.01)
 
-        """torch.nn.init.constant_(self.lin1.weight, 0.001)
-        torch.nn.init.constant_(self.lin2.weight, 0.001)
-        torch.nn.init.constant_(self.lin3.weight, 0.001)
-        torch.nn.init.constant_(self.lin4.weight, 0.001)"""
+        #torch.nn.init.constant_(self.lin1.weight, 0.001)
+        #torch.nn.init.constant_(self.lin2.weight, 0.001)
+        #torch.nn.init.constant_(self.lin3.weight, -0.001)
+        #torch.nn.init.constant_(self.lin4.weight, -0.001)
 
         self.bn1 = nn.BatchNorm1d(H1) # (batch) normalization after affine
         self.bn2 = nn.BatchNorm1d(output_size)
@@ -71,14 +71,15 @@ class Model(nn.Module):
         ###############################################################################################################
         # x = torch.sigmoid(self.lin1(x)) # layer 1
 
-        x = torch.sigmoid(self.bn1(self.lin1(x))) # layer 2
-        x = torch.sigmoid(self.lin2(x))
+        # x = torch.sigmoid(self.bn1(self.lin1(x))) # layer 2
+        # x = torch.sigmoid(self.lin2(x))
 
-        # x = self.bn1(self.lin1(x)) # layer 4
-        # x = self.bn1(self.lin2(x))
-        # x = self.bn1(self.lin3(x))
-        # x = self.bn2(self.lin4(x))
-        # x = torch.sigmoid(x)
+        x = self.bn1(self.lin1(x)) # layer 4
+        x = self.bn1(self.lin2(x))
+        x = self.bn1(self.lin3(x))
+        x = self.bn2(self.lin4(x))
+        x = torch.sigmoid(x)
+        # x = torch.nn.functional.softmax(x) # none
         ###############################################################################################################
         #                                              END OF YOUR CODE                                               #
         ###############################################################################################################
@@ -98,12 +99,12 @@ model = Model(2, 2, 1)
 #                  TODO : 손실함수(BCELoss)와 optimizer(Adam)를 정의(learning rate=0.01)                     #
 ##############################################################################################################
 BCELoss = nn.BCELoss() # cross entropy for calculating loss func
-optimizer = torch.optim.Adam(model.parameters(), lr = 0.2) # for learning
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.01) # for learning
 ###############################################################################################################
 #                                              END OF YOUR CODE                                               #
 ###############################################################################################################
 
-epochs = 2000 # how many
+epochs = 200000 # how many
 losses = [] # store record of loss
 
 # batch train step
